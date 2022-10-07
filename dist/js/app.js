13c0314e42482cd5f3ac25c4b48edbe0381c4641
Targonkp Targonkp <targon_k_p@mail.ru>
2022-10-08 00:35:39 +0300

    video-player added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,72 @@
 "use strict";
 
-//замедляю видео
-var videoEl = document.querySelector('.background-video');
-videoEl.playbackRate = 0.4; //получаю список элементов с количеством комнат в фильтре и назначаю класс при клике
+//получаю все видео и устанавливаю на паузу
+var videoElList = document.querySelectorAll('.background-video');
+videoElList.forEach(function (element) {
+  element.pause();
+}); //массив с h1
+
+var headerArr = ['Стильная архитектура и виды из окон будущей квартиры не оставят равнодушных', 'Большие парковые территории и безопасные дворы созданы для счастливой жизни']; //записываю начальное значение активного видео
+
+var startVideo = 0; //функция, запускающая видео с активным классом
+
+function activeVideo() {
+  //получаю видео с активным классом
+  var videoActive = document.querySelector('.background-video-active'); //включаю видео
+
+  videoActive.play(); //замедляю видео
+
+  videoActive.playbackRate = 0.4;
+}
+
+activeVideo(); //функция, увеличивающая значение (правая стрелка)
+
+function videoBackgroundNext() {
+  //делаю слайдер непрерывным
+  if (startVideo === videoElList.length - 1) {
+    startVideo = 0;
+  } else {
+    startVideo += 1;
+  } //удаляю у всех плееров активный класс
+
+
+  videoElList.forEach(function (element) {
+    element.classList.remove('background-video-active');
+  }); //добавляю следующему плееру активный класс
+
+  videoElList[startVideo].classList.add('background-video-active');
+  activeVideo();
+} //функция, уменьшающая значение (левая стрелка)
+
+
+function videoBackgroundPrevious() {
+  if (startVideo === 0) {
+    startVideo = videoElList.length - 1;
+  } else {
+    startVideo -= 1;
+  }
+
+  console.log(startVideo); //удаляю у всех плееров активный класс
+
+  videoElList.forEach(function (element) {
+    element.classList.remove('background-video-active');
+  }); //добавляю предыдущему плееру активный класс
+
+  videoElList[startVideo].classList.add('background-video-active');
+  activeVideo();
+} // function func(){
+//     videoEl.pause();
+//     sourceVideoEl.src = videoArr[1];
+//     videoEl.load();
+//     videoEl.play();
+//     videoEl.playbackRate = 0.4;
+// }
+// let videoArr = ['video/Dog%20-%2078200.mp4', 'video/Flowers%20-%2064035.mp4'];
+//навешиваю функциb для изменения фона
+
+
+document.querySelector('.switch-arrow__right').addEventListener('click', videoBackgroundNext);
+document.querySelector('.switch-arrow__left').addEventListener('click', videoBackgroundPrevious); //получаю список элементов с количеством комнат в фильтре и назначаю класс при клике
 
 var roomsList = document.querySelectorAll('.rooms-element');
 roomsList.forEach(function (element) {
